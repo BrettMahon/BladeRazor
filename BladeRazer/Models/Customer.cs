354699751f7e209a6735d7e45c0f2c5905849f62
Brett Mahon <brett2k@gmail.com>
2020-05-19 09:49:44 +0200

    Add project files.

---
+++ BladeRazer/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BladeRazer.Models
+{
+
+    public enum CustomerType
+    {
+        Cash = 1,
+        Credit = 2
+    }
+
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        
+        public bool Active { get; set; }
+        
+        public string Telephone { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Address { get; set; }
+        
+        public CustomerType CustomerType { get; set; }
+        
+        [Display(Name = "Name")]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
---
+++ BladeRazer/Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BladeRazer.Models
+{
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Approved = 2,
+        Paid = 3,
+        Delivered = 4
+    }
+
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Product Name")]
+        public string  ProductName { get; set; }
+        
+        [Display(Name = "Product Code")]
+        public int ProductCode { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        
+        [Display(Name = "Order Status")]
+        public OrderStatus OrderStatus { get; set; }
+
+        [Display(Name = "Customer")]
+        public int CustomerId { get; set; }
+        
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+    }
+}
