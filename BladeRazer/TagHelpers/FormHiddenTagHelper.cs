cc4b5e3f30fdcc5c33233731f70fe770b50fb784
Brett Mahon <brett2k@gmail.com>
2020-05-27 11:56:12 +0200

    HiddenTagHelper

--- BladeRazer/TagHelpers/FormHiddenTagHelper.cs
+++ BladeRazer/TagHelpers/FormHiddenTagHelper.cs
@@ -8,6 +8,14 @@ namespace BladeRazor.TagHelpers
     {
         public FormHiddenTagHelper(IHtmlGenerator generator, IStyles styles = null) : base(generator, styles) { }
 
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            
+            output.Content.AppendHtml(GenerateTagHelper());
+        }
+
         protected override TagHelperOutput GenerateTagHelper() => tg.GenerateHiddenTagHelper(For);
 
     }
